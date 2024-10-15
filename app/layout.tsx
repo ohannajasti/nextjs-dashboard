8ac52eab4561d55c915ce035e6103d2e79bdf0c1
jasti.ohanna <jasti.ohanna@krom.id>
2024-10-15 16:58:24 +0700

    feat: chapter 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+// How to add global variable css in Next.js
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body> 
     </html>
   );
 }
+// The term antialiased is often used in CSS to improve the appearance of the edges of solid shapes.
+//  Any UI you add to the root layout will be shared across all pages in your application. You can use the root layout to modify your <html> and <body> tags, and add metadata
+
